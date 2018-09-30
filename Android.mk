4813c8b3302edcdd1f0d34ddbe751449d715bc4f
Andrew <poisk567@gmail.com>
2018-09-30 19:58:23 +0300

    add missing files

--- Android.mk
+++ Android.mk
@@ -22,13 +22,21 @@ ifeq ($(MTKPATH),)
 # Proprietary Modules go here
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libdpframework
-LOCAL_MODULE_OWNER := sony
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE := libaudiocustparam
+LOCAL_SRC_FILES_64 := vendor/lib64/libaudiocustparam.so
+LOCAL_SRC_FILES_32 := vendor/lib/libaudiocustparam.so
+LOCAL_MULTILIB := both
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MULTILIB := 32
-LOCAL_SRC_FILES_32 := proprietary/lib/libdpframework.so
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libmtkcam_fwkutils
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MULTILIB := both
+LOCAL_SRC_FILES_64 := vendor/lib64/libmtkcam_fwkutils.so
+LOCAL_SRC_FILES_32 := vendor/lib/libmtkcam_fwkutils.so
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -37,8 +45,9 @@ LOCAL_MODULE_OWNER := sony
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MULTILIB := 64
-LOCAL_SRC_FILES_64 := proprietary/lib64/libdpframework.so
+LOCAL_MULTILIB := both
+LOCAL_SRC_FILES_32 := lib/libdpframework.so
+LOCAL_SRC_FILES_64 := lib64/libdpframework.so
 include $(BUILD_PREBUILT)
 
 endif
