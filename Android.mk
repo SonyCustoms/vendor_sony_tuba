d57c86682dfd174a96a9e636754a4d7e82945cd4
Andrew <poisk567@gmail.com>
2018-09-30 19:39:19 +0300

    fixes

--- Android.mk
+++ Android.mk
@@ -22,26 +22,6 @@ ifeq ($(MTKPATH),)
 # Proprietary Modules go here
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libaudiopolicymanager
-LOCAL_MODULE_OWNER := sony
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MULTILIB := 32
-LOCAL_SRC_FILES_32 := proprietary/lib/libaudiopolicymanager.so
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libaudiopolicymanager
-LOCAL_MODULE_OWNER := sony
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MULTILIB := 64
-LOCAL_SRC_FILES_64 := proprietary/lib64/libaudiopolicymanager.so
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := libdpframework
 LOCAL_MODULE_OWNER := sony
 LOCAL_MODULE_TAGS := optional
