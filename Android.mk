281a19a89ce2d7fab7dbd25bc0b7391cd7080a23
Andrew <poisk567@gmail.com>
2018-10-31 13:08:49 +0200

    add camera_wrapper

--- Android.mk
+++ Android.mk
@@ -60,6 +60,26 @@ LOCAL_SRC_FILES_32 := lib/libdpframework.so
 LOCAL_SRC_FILES_64 := lib64/libdpframework.so
 include $(BUILD_PREBUILT)
 
+ifeq ($(USE_MTK_CAMERA_WRAPPER),true)
+include $(CLEAR_VARS)
+LOCAL_MODULE := libcam2halsensor
+LOCAL_SRC_FILES_64 := proprietary/lib64/libcam2halsensor.so
+LOCAL_SRC_FILES_32 := proprietary/lib/libcam2halsensor.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libcameracustom
+LOCAL_SRC_FILES_64 := proprietary/lib64/libcameracustom.so
+LOCAL_SRC_FILES_32 := proprietary/lib/libcameracustom.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+endif
+
 endif
 endif
 
