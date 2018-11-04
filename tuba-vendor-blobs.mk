f3d6c32ee9a75e1b79003627f8108f92c84e07ea
Andrew <poisk567@gmail.com>
2018-11-04 07:37:37 +0200

    use prebuild keylayout

--- tuba-vendor-blobs.mk
+++ tuba-vendor-blobs.mk
@@ -3,4 +3,5 @@ PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/tuba/etc,syste
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/tuba/framework,system/framework)
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/tuba/lib,system/lib)
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/tuba/lib64,system/lib64)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/tuba/usr,system/usr)
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/tuba/vendor,system/vendor)
