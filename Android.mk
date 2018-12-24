de40c4b6f0f8757b5b5a62a0239a61f014933fa1
Andrew <poisk567@gmail.com>
2018-12-24 13:47:57 +0200

    try to fix ril again and again..

--- Android.mk
+++ Android.mk
@@ -23,8 +23,8 @@ ifeq ($(MTKPATH),)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libaudiocustparam
-LOCAL_SRC_FILES_64 := vendor/lib64/libaudiocustparam.so
-LOCAL_SRC_FILES_32 := vendor/lib/libaudiocustparam.so
+LOCAL_SRC_FILES_64 := lib64/libaudiocustparam.so
+LOCAL_SRC_FILES_32 := lib/libaudiocustparam.so
 LOCAL_MULTILIB := both
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
@@ -35,8 +35,8 @@ LOCAL_MODULE := libmtkcam_fwkutils
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MULTILIB := both
-LOCAL_SRC_FILES_64 := vendor/lib64/libmtkcam_fwkutils.so
-LOCAL_SRC_FILES_32 := vendor/lib/libmtkcam_fwkutils.so
+LOCAL_SRC_FILES_64 := lib64/libmtkcam_fwkutils.so
+LOCAL_SRC_FILES_32 := lib/libmtkcam_fwkutils.so
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -46,8 +46,8 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MULTILIB := both
-LOCAL_SRC_FILES_32 := vendor/lib/libdpframework.so
-LOCAL_SRC_FILES_64 := vendor/lib64/libdpframework.so
+LOCAL_SRC_FILES_32 := lib/libdpframework.so
+LOCAL_SRC_FILES_64 := lib64/libdpframework.so
 include $(BUILD_PREBUILT)
 
 endif
