c4090ca0fac75baeb9f492ba44bc1a7dec495bbf
Andrew <poisk567@gmail.com>
2019-01-16 20:04:07 +0200

    little cleanup

--- Android.mk
+++ Android.mk
@@ -23,8 +23,8 @@ ifeq ($(MTKPATH),)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libaudiocustparam
-LOCAL_SRC_FILES_64 := lib64/libaudiocustparam.so
-LOCAL_SRC_FILES_32 := lib/libaudiocustparam.so
+LOCAL_SRC_FILES_64 := vendor/lib64/libaudiocustparam.so
+LOCAL_SRC_FILES_32 := vendor/lib/libaudiocustparam.so
 LOCAL_MULTILIB := both
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
@@ -35,8 +35,8 @@ LOCAL_MODULE := libmtkcam_fwkutils
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MULTILIB := both
-LOCAL_SRC_FILES_64 := lib64/libmtkcam_fwkutils.so
-LOCAL_SRC_FILES_32 := lib/libmtkcam_fwkutils.so
+LOCAL_SRC_FILES_64 := vendor/lib64/libmtkcam_fwkutils.so
+LOCAL_SRC_FILES_32 := vendor/lib/libmtkcam_fwkutils.so
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
