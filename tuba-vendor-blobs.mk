416e58a9466af615cf7d30740a94a9749cf998ef
Andrew <poisk567@gmail.com>
2019-01-18 18:11:46 +0200

    init for oreo

--- tuba-vendor-blobs.mk
+++ tuba-vendor-blobs.mk
@@ -1,6 +1,9 @@
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/tuba/bin,system/bin)
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/tuba/etc,system/etc)
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/tuba/lib,system/lib)
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/tuba/lib64,system/lib64)
+
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/tuba/usr,system/usr)
+
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/tuba/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/tuba/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/tuba/lib64,system/lib64)
