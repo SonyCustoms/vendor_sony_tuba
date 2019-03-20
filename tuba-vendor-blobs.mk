7fb3ea75719f09ad88cc38528cdaa4dee346c566
Andrew <poisk567@gmail.com>
2019-03-20 07:38:30 +0200

    mad

--- tuba-vendor-blobs.mk
+++ tuba-vendor-blobs.mk
@@ -4,4 +4,3 @@ PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/tuba/lib,syste
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/tuba/lib64,system/lib64)
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/tuba/usr,system/usr)
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/tuba/vendor,system/vendor)
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/tuba/framework,system/framework)
